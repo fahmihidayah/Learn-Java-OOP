7117d5022ba441ff28a5453deff2edb8e3dd4307
fahmi-mac <f.hidayah@pixilapps.com>
2024-03-11 12:42:58 +0700

    tugas main

---
+++ README.md
@@ -0,0 +1,4 @@
+Daftar Tugas : 
+
+
+https://github.com/fahmihidayah/Learn-Java-OOP/tree/main/tugas_1 
--- fahmi/MainApp.java
+++
@@ -1,7 +0,0 @@
-package fahmi;
-
-public class MainApp {
-    public static void main(String[] args) {
-        
-    }
-}
